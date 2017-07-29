71f08f3450df9b3d3d6ff66695d41c21ff0befab
Ketty Duclos <kettyduclos@yahoo.com>
2017-07-29 16:16:32 -0400

    my first commit

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,17 @@
+import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
+
+var mobileMenu = new MobileMenu();
+new RevealOnScroll($('.work-item'), "85%");
+new RevealOnScroll($('.testimonials'), "60%");
+var stickyHeader = new StickyHeader();
+var modal = new Modal();
+
+ 
+ 
+ 
+ 
+ 
